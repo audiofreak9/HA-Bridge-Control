a4f96bbba49945a36fd8ec12d4908f58899fd4fc
audiofreak9 <audiofreak9@gmail.com>
2016-01-17 21:12:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
+extract($_POST);
 $heyuvar = "heyu -c /home/pi/.heyu/x10config ";
-if (isset($_POST['hu'])) {
-if (isset($_POST['action'])) $command = $heyuvar . $_POST['action'] . ' ' . $_POST['hu'] . ' 2>&1';
-if (isset($_POST['bd'])) {
-$level = 22 - round($_POST['percent'] * (22 / 100));
-if ($level == 0) $level = 1;
-$command = $heyuvar . $_POST['bd'] . ' ' . $_POST['hu'] . ' ' . $level . ' > /dev/null 2>/dev/null &';
+$heyuend = " > /dev/null 2>/dev/null &";
+if((isset($hu)) && (isset($action))) {
+        if (is_numeric($action)) {
+                $level = (22 - round($action * (22 / 100)) == 0) ? 1 : 22 - round($action * (22 / 100));
+                $heyuvar .= 'obdim ' . $hu . ' ' . $level . $heyuend;
+        }else{
+                $heyuvar .= $action . ' ' . $hu . $heyuend;
+        }
+        exec($heyuvar);
+        header("Location: /");
+        exit;
 }
-$output = shell_exec($command);
-header("Location: /");
-exit;
-}
-if (isset($_POST['sc'])) {
-$command = $heyuvar . $_POST['sc'] . ' > /dev/null 2>/dev/null &';
-$output = shell_exec($command);
-header("Location: /");
-exit;
+if (isset($sc)) {
+        $heyuvar .= $sc . $heyuend;
+        exec($heyuvar);
+        header("Location: /");
+        exit;
 }
 ?>
 <!DOCTYPE html>
@@ -28,21 +30,18 @@ exit;
 <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 <link href="startup.png" media="(device-width: 320px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
 <link rel="apple-touch-icon" href="x10switch_icon.png"/>
-<title>Lighting</title>
+<title>X10 Lighting</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 <link rel="stylesheet" href="bootstrap-responsive.css">
 <link rel="stylesheet" href="bootstrap-theme.min.css">
 </head>
 <body>
-<br />
 <div class="container container-fluid" role="main">
-<div class="row">
-<div class="col-md-6">
-<div class="panel panel-default">
-<div class="panel-heading">
-<h3 class="panel-title">Devices</h3>
-</div>
-<div class="panel-body">
+        <div class="row">
+                <div class="col-md-6">
+                        <div class="panel panel-default">
+                                <div class="panel-heading">Devices</div>
+                                <div class="panel-body">
 <?php
 $get_alias = $heyuvar . 'show alias';
 $devices = explode(":", preg_replace('/( )+/', ' ', str_replace("alias", ":", str_replace("[Aliases]", "", shell_exec($get_alias)))) . ":");
@@ -56,36 +55,42 @@ if ($dev_address) {
 $dev_level = trim(shell_exec($get_level));
 if (($dev_type != "StdLM") && ($dev_level > 0)) $dev_level = 100;
 ?>
-<h5><span class="label label-default"><?php echo $dev_address . ' ' . ucwords(str_replace("_", " ", $dev_name)); ?></span></h5>
-<div style="margin:0 0 4px 0">
-<form class="form-inline" method="post" action="">
-<input type="hidden" name="hu" value="<?php echo $dev_address; ?>" />
-<button type="submit" class="btn btn-sm1 btn-success" name="action" value="on">on</button>
-<button type="submit" class="btn btn-sm1 btn-danger" name="action" value="off">off</button>
-<?php if ($dev_type == "StdLM") { ?>
-<button type="submit" class="btn btn-sm1 btn-default" name="bd" value="dim">dim</button>
-<button type="submit" class="btn btn-sm1 btn-default" name="bd" value="bright">brt</button>
-<input class="form-control" type="text" value="10%" style="width:56px" />
-<input class="form-control" name="percent" type="range" min="10" max="90" step="10" value="10" />
-<?php } ?>
-</form>
-</div>
-<div class="progress">
-<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="<?php echo $dev_level; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $dev_level; ?>%"><?php echo ' ' . $dev_level . '% '; ?></div>
-</div>
+
+                                        <form class="form-inline" id="form<?php echo $dev_address; ?>" method="post" action="">
+                                        <input type="hidden" name="hu" value="<?php echo $dev_address; ?>" />
+                                        <button type="submit" class="btn btn-sm1 btn-success" name="action" value="on">On</button>
+                                        <button type="submit" class="btn btn-sm1 btn-danger" name="action" value="off">Off</button>
+<?php
+if ($dev_type == "StdLM") { 
+        for ($x = 30; $x < 100; $x+=10) { ?>
+                                        <button type="submit" class="btn btn-sm1 btn-default" name="action" value="<?php echo $x; ?>"><?php echo $x; ?></button>
+<?php
+        }
+}
+?>
+                                        </form>
+                                        </form>
+                                        <div style="margin-top:4px">
+                                                <div class="col-xs-3 label label-info"><?php echo ucwords(str_replace("_", " ", $dev_name)); ?></div>
+                                                <div class="col-xs-9">
+                                                        <div class="progress">
+                                                                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php echo $dev_level; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $dev_level; ?>%">
+                                                                        <?php echo ' ' . $dev_level . '% '; ?>
+                                                                </div>
+                                                        </div>
+                                                </div>
+                                        </div>
 <?php
 }
 if($count == ceil(count($devices)/2)) {
 ?>
 </div>
-</div>
-</div>
-<div class="col-md-6">
-<div class="panel panel-default">
-<div class="panel-heading">
-<h3 class="panel-title">Devices</h3>
-</div>
-<div class="panel-body">
+                        </div>
+                </div>
+                <div class="col-md-6">
+                        <div class="panel panel-default">
+                                <div class="panel-heading">Devices</div>
+                                <div class="panel-body">
 <?php
 }
 }
@@ -101,37 +106,29 @@ $get_scenes = $heyuvar . 'show scene';
 $scenes = explode(":", preg_replace('/( )+/', ' ', str_replace("scene", ":", str_replace("[Scenes]", "", shell_exec($get_scenes)))) . ":");
 if (!empty($scenes)) {
 ?>
-<form class="myform" method="post" action="">
-<div class="span4">
-<select class="form-control" name="sc">
-<option value="">Select Scene</option>
+                                        <form class="myform" method="post" action="">
+                                                <div class="span4">
+                                                        <select class="form-control" name="sc">
+                                                                <option value="">Select Scene</option>
 <?php
 foreach ($scenes as &$scene) {
 list($sc_name, $sc_action, $sc_group, $sc_action2, $sc_dim, $sc_dim2) = explode(":", str_replace(" ", ":", trim($scene)));
 if ($sc_name) echo "<option value=\"$sc_name\">" . ucwords(str_replace("_", " ", $sc_name)) . "</option>";
 }
 ?>
-</select>
-</div>
+                                                        </select>
+                                                </div>
 <?php
 }
 ?>
-<input type="submit" value="Submit" class="btn btn-sm1 btn-default" />
-</div>
-</form>
-</div>
-</div>
-</div>
-</div>
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js?ver=CDN"></script>
+                                                <input type="submit" value="Submit" class="btn btn-sm1 btn-default" />
+                                                </div>
+                                        </form>
+                                </div>
+                        </div>
+                </div>
+        </div>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js?ver=CDN"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<script>
-$(document).ready(function(){
-$("[type=range]").change(function(){
-var newv=$(this).val() + '%';
-$(this).prev().val(newv);
-});
-});
-</script>
 </body>
 </html>
