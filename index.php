120c6dc268e878fa69cc2fbd96942f58d061938c
audiofreak9 <audiofreak9@gmail.com>
2016-04-29 10:41:52 -0400

    HA-Bridge Control
    Now uses the ha-bridge’s devices and states for control

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
-<?php
-extract($_POST);
-$heyuvar = "heyu -c /home/pi/.heyu/x10config ";
-$heyuend = " > /dev/null 2>/dev/null &";
-if((isset($hu)) && (isset($action))) {
-        if (is_numeric($action)) {
-                $level = (22 - round($action * (22 / 100)) == 0) ? 1 : 22 - round($action * (22 / 100));
-                $heyuvar .= 'obdim ' . $hu . ' ' . $level . $heyuend;
-        }else{
-                $heyuvar .= $action . ' ' . $hu . $heyuend;
-        }
-        exec($heyuvar);
-        header("Location: /");
-        exit;
-}
-if (isset($sc)) {
-        $heyuvar .= $sc . $heyuend;
-        exec($heyuvar);
-        header("Location: /");
-        exit;
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,104 +8,107 @@ if (isset($sc)) {
 <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 <link href="images/startup.png" media="(device-width: 320px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image">
 <link rel="apple-touch-icon" href="images/x10switch_icon.png"/>
-<title>X10 Lighting</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<title>X10 Bridge Control</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/bootstrap-responsive.css">
 <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+<style>
+body{margin:0;padding:10px 0 0 0}
+.l{display:none}
+</style>
 </head>
 <body>
-<div class="container container-fluid" role="main">
-        <div class="row">
-                <div class="col-md-6">
-                        <div class="panel panel-default">
-                                <div class="panel-heading">Devices</div>
-                                <div class="panel-body">
+<div class="container" role="main">
+        <div class="panel panel-default">
+                <div class="panel-heading">Devices</div>
+                <div class="panel-body">
 <?php
-$get_alias = $heyuvar . 'show alias';
-$devices = explode(":", preg_replace('/( )+/', ' ', str_replace("alias", ":", str_replace("[Aliases]", "", shell_exec($get_alias)))) . ":");
-if (!empty($devices)) {
-$count = 0;
-foreach ($devices as &$device) {
-$count++;
-list($dev_name, $dev_address, $dev_type) = explode(":", str_replace(" ", ":", trim($device)));
-$get_level = $heyuvar . 'dimlevel ' . $dev_address;
-if ($dev_address) {
-$dev_level = trim(shell_exec($get_level));
-if (($dev_type != "StdLM") && ($dev_level > 0)) $dev_level = 100;
+//Server Name variable
+$SN = $_SERVER['SERVER_NAME'];
+$rm = array($SN, "http://localhost/", "echo.php?", "action", "hu", "off", "=", "&");
+//Get the devices from the bridge, put them into an array
+$ha_devices = json_decode(file_get_contents("http://$SN:8080/api/devices"), true);
+$deviceTypes = array();
+//Get each device type into an array
+foreach ($ha_devices as $ha_device) $deviceTypes[] = $ha_device["deviceType"];
+//Sort by device type to group similar
+array_multisort($deviceTypes, SORT_DESC, $ha_devices);
+//Determine half the device count
+$halfval = ceil(count($ha_devices) / 2);
+for($x = 0; $x <= count($ha_devices) - 1; $x++) {
+        $dev_level = round(($ha_devices[$x]["deviceSetValue"] / 255)*100);
 ?>
-                                        <form class="form-inline" id="form<?php echo $dev_address; ?>" method="post">
-                                        <input type="hidden" name="hu" value="<?php echo $dev_address; ?>" />
-                                        <button type="submit" class="btn btn-sm1 btn-success" name="action" value="on">On</button>
-                                        <button type="submit" class="btn btn-sm1 btn-danger" name="action" value="off">Off</button>
-<?php
-if ($dev_type == "StdLM") { 
-        for ($x = 30; $x < 100; $x+=10) { ?>
-                                        <button type="submit" class="btn btn-sm1 btn-default" name="action" value="<?php echo $x; ?>"><?php echo $x; ?></button>
-<?php
-        }
-}
-?>
-                                        </form>
-                                        <div style="margin-top:4px">
-                                                <div class="col-xs-3 label label-info"><?php echo ucwords(str_replace("_", " ", $dev_name)); ?></div>
-                                                <div class="col-xs-9">
-                                                        <div class="progress">
-                                                                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?php echo $dev_level; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $dev_level; ?>%">
-                                                                        <?php echo ' ' . $dev_level . '% '; ?>
-                                                                </div>
-                                                        </div>
+                <div class="col-sm-4">
+                        <div class="row-fluid">
+                                <form class="form-inline" id="<?php echo $ha_devices[$x]["id"]; ?>">
+                                        <div class="col-sm-12 col-md-6">
+                                                <div class="label label-info col-xs-12" style="padding-top:4px">
+                                                        <?php echo str_replace(". ", ".", ucwords(str_replace(".", ". ", $ha_devices[$x]["name"]))); ?>
                                                 </div>
                                         </div>
-<?php
-}
-if($count == ceil(count($devices)/2)) {
-?>
-                                </div>
-                        </div>
-                </div>
-                <div class="col-md-6">
-                        <div class="panel panel-default">
-                                <div class="panel-heading">Devices</div>
-                                <div class="panel-body">
-<?php
-}
-}
-}
-?>
-                                </div>
-<?php
-$get_scenes = $heyuvar . 'show scene';
-$scenes = explode(":", preg_replace('/( )+/', ' ', str_replace("scene", ":", str_replace("[Scenes]", "", shell_exec($get_scenes)))) . ":");
-if ((!empty($scenes)) && (trim($scenes[0]) != "-none-")) {
-?>
-                                <div class="panel-heading">Scenes</div>
-                                <div class="panel-body">
-                                        <form class="myform" method="post" action="">
-                                        <div class="span4">
-                                                <select class="form-control" name="sc">
-                                                        <option value="">Select Scene</option>
-<?php
-        foreach ($scenes as &$scene) {
-                list($sc_name, $sc_action, $sc_group, $sc_action2, $sc_dim, $sc_dim2) = explode(":", str_replace(" ", ":", trim($scene)));
-                if ($sc_name) echo "<option value=\"$sc_name\">" . ucwords(str_replace("_", " ", $sc_name)) . "</option>";
-        }
-?>
-                                                </select>
+                                        <div class="col-sm-12 col-md-6">
+                                                <button type="submit" class="btn btn-xs btn-success" name="action" value="on">On</button>
+                                                <button type="submit" class="btn btn-xs btn-danger" name="action" value="off">Off</button>
+                                                &nbsp;<span class="badge l" id="l<?php echo $ha_devices[$x]["id"]; ?>"><?php echo $dev_level . '%'; ?></span>
+                                         </div>
+                                        <div class="col-sm-12 col-md-12" style="margin-top:10px;height:15px">
+                                                <input class="ps" type="<?php echo ((strpos($ha_devices[$ha_val]["onUrl"],"percent") > 0)||($ha_devices[$x]["dimUrl"]))? "range" : "hidden" ; ?>" min="1" max="100" step="1" value="<?php echo $dev_level; ?>" />
+                                                <input type="hidden" id="sl<?php echo $ha_devices[$x]["id"]; ?>" value="100" />
                                         </div>
-                                        <input type="submit" value="Submit" class="btn btn-sm1 btn-default" /> 
-                                        </form>
-                                 </div>
-<?php
-}
-?>
-                                        <input type="submit" value="Submit" class="btn btn-sm1 btn-default" />
+                                        <div class="col-sm-12 col-md-12">
+                                                <div class="progress">
+                                                        <div class="progress-bar progress-bar-success progress-bar-striped" id="prog<?php echo $ha_devices[$x]["id"]; ?>" role="progressbar" aria-valuenow="<?php echo $dev_level; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $dev_level; ?>%">
+                                                               <?php echo $dev_level . '%'; ?>
+                                                        </div>
+                                                </div>
                                         </div>
                                 </form>
                         </div>
                 </div>
+<?php
+}
+?>
+                </div>
         </div>
 </div>
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js?ver=CDN"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<script>
+$(".btn").click(function() {
+  var dev_id = $(this).closest("form").attr('id');
+  var dev_val = $(this).val();
+  var dev_per = $('#sl' + dev_id).attr('value');
+  var data = '{"on":';
+  if(dev_val == "off") {
+    percent = 0;
+    data += 'false';
+  }else{
+    percent = dev_per;
+    data += 'true';
+    data += ', "bri":' + Math.round(2.55 * dev_per);
+  }
+  data += '}';
+  $.ajax({
+    type: 'POST',
+    dataType: 'json',
+    url: 'http://<?php echo $SN; ?>:8080/api/c/lights/' + dev_id + '/state',
+    headers: {"X-HTTP-Method-Override": "PUT"},
+    data: data,
+    success : updateProgress(percent, dev_id)
+  });
+  return false;
+});
+$('.ps').on("change", function() {
+    var my_id = $(this).closest("form").attr('id');
+    var level = $(this).val();
+    $('#sl' + my_id).val(level);
+    $('#l' + my_id).html(level + '%').fadeIn( 400 ).delay( 800 ).fadeOut( 400 ); 
+});
+function updateProgress(percent, dev_id){
+    if(percent > 100) percent = 100;
+    $('#prog' + dev_id).css('width', percent+'%');
+    $('#prog' + dev_id).html(percent+'%');
+}
+</script>
 </body>
 </html>
